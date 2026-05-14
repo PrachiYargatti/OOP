e7004f9a402c443f03ce40b64150db29b3dc2b0f
Prachi Gajanan Yargatti <yaragattiprachi@gmail.com>
2026-05-14 11:41:03 +0530

    Create powerSet.cpp

---
+++ powerSet.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:
+    vector<vector<int>> powerSet(vector<int>& arr) {
+        vector<vector<int>> ans;
+        int n = arr.size();
+        for (int num = 0; num < (1 << n); num++) {
+            vector<int> temp;
+            for (int i = 0; i < n; i++) {
+                if (num & (1 << i)) {
+                    temp.push_back(arr[i]);
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+
+};
