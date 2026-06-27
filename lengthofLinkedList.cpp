faa926940779355a9a0f7fd4330ae5bbfcd3297f
Prachi Gajanan Yargatti <yaragattiprachi@gmail.com>
2026-06-27 21:24:03 +0530

    Create lengthofLinkedList.cpp

---
+++ lengthofLinkedList.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+class Node {
+    public:
+        int data;
+        Node* next;
+    public:
+        Node(int value, Node* address){
+            data = value;
+            next = address;
+        }
+    public:
+        Node(int value){
+            data = value;
+            next = NULL;
+        }
+};
+*/
+
+class solution {
+public:
+    int lengthofLL(Node* head) {
+        Node* temp = head;
+        int count=0;
+        while(temp != nullptr){
+            count++;
+            temp=temp->next;
+        }
+        return count;
+        
+    }
+
+};
