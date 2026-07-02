4508b66ce1e19e90e1b226d1bfff91c4752cb55a
Prachi Gajanan Yargatti <yaragattiprachi@gmail.com>
2026-07-02 15:21:56 +0530

    Add arrayToDLL function in arrayToDoubleLinkedList.cpp
    Implement a function to convert an array to a doubly linked list.

---
+++ arrayToDoubleLinkedList.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+class Node {
+    public:
+        int data;
+        Node* next;
+        Node* prev;
+        Node(int value, Node* next_Address, Node* prev_Address){
+            data = value; 
+            next = next_Address; 
+            prev = prev_Address;
+        }
+        Node(int value){
+           data = value;
+           next = nullptr;
+           prev = nullptr;
+        }
+};
+*/
+
+class solution {
+public:
+    Node* arrayToDLL(vector<int>& arr) {
+        int n = arr.size();
+        if (n == 0) return nullptr;
+        Node* head = new Node(arr[0]);
+        Node* cur = head;
+        
+        for(int i=1;i<=n-1;i++){
+            Node* temp = new Node(arr[i]);
+            temp->prev = cur;
+            cur->next = temp;
+            cur = temp;
+        }
+        
+        return head;
+    }
+
+};
