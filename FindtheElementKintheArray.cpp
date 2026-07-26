89bd43e41ccccc906de26f7bd9f138ddb99c2a6f
Prachi Gajanan Yargatti <yaragattiprachi@gmail.com>
2026-07-26 11:31:24 +0530

    Create FindtheElementKintheArray.cpp

---
+++ FindtheElementKintheArray.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+public:
+    int search(int arr[], int n, int k){
+        for(int i=0;i<n;i++){
+            if(arr[i] == k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
