480d26b5341464321eb14dae32000754bd21015b
Prachi Gajanan Yargatti <yaragattiprachi@gmail.com>
2026-07-28 21:02:38 +0530

    Implement kthMissingNumber function in C++

---
+++ kthMissingNumberInArray.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:
+    int kthMissingNumber(vector<int> &arr, int k) {
+        int low = 0, high = arr.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int missing = arr[mid] - (mid + 1);
+            
+            if (missing < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return low + k;
+   }
+};
