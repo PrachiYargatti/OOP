26029cd74d4806d8b80d6d760b9d2f0f8708b610
Prachi Gajanan Yargatti <yaragattiprachi@gmail.com>
2026-08-13 12:02:21 +0530

    Create queueReversal.cpp

---
+++ queueReversal.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+    public:
+     queue<int> rev(queue<int> q) {
+        stack<int> s;
+        
+        while(!q.empty()){
+            s.push(q.front());
+            q.pop();
+        }
+        
+        while(!s.empty()){
+            q.push(s.top());
+            s.pop();
+        }
+        
+        return q;
+     }
+};
