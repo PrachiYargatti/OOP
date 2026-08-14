ae4136104fac45e1fe29fcaff6005a6891ea54e3
Prachi Gajanan Yargatti <yaragattiprachi@gmail.com>
2026-08-14 11:05:05 +0530

    Create isLeapYear.cpp

---
+++ isLeapYear.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+    public:
+    bool isLeapYear(int year) {
+        if((year%400 == 0) || (year%4 == 0 && year%100 != 0)){
+            return true;
+        }
+        return false;
+      
+  }
+};
